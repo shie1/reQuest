a37d0e73019f108e45a47e5bb1053c56da3ea62c
Shie1 <sonkolybence@outlook.hu>
2021-05-30 22:13:23 +0200

    Added options and some other stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, webbrowser, sys, socketserver, socket, pytube, unicodedata, re, urllib, http.server, platform, slugi, pystray
+import os, webbrowser, sys, socketserver, socket, pytube, unicodedata, re, urllib, http.server, platform, slugi, pystray, getopt
 from threading import Thread
 from time import sleep
 from pyngrok.ngrok import connect as cn
@@ -6,31 +6,41 @@ from PIL import Image
 
 iconfile = Image.open("icon.png")
 
-if(len(sys.argv) > 1):
-    try:
-        port = int(sys.argv[1])
-    except:
-        port = 4658
-else:
-    port = 4658
+opts, args = getopt.getopt(sys.argv[1:],"p:",["port=","no-init"])
 
-slugi.init(cn(port))
+no_init = False
+port = 4658
+
+for opt, arg in opts:
+    if opt in ('-p',"--port"):
+        port = int(arg)
+    elif opt in ("--no-init"):
+        no_init = True
+
+if no_init == False:
+    slugi.init(cn(port))
 
 os.chdir('web')
 
 if(platform.system() == "Windows"):
+    def openPath(path):
         os.system("explorer.exe " + path)
+    def clear():
+        os.system("cls")
 elif(platform.system() == "Linux"):
     def openPath(path):
         os.system("xdg-open " + path)
+    def clear():
+        os.system("clear")
 elif(platform.system() == "Darwin"):
     def openPath(path):
         os.system("open " + path)
+    def clear():
+        os.system("clear")
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         req = self.requestline.split(" ")[1]
-        print(self.path)
         try:
             nparams = req.split('?')[1].split('&')
         except:
@@ -71,7 +81,12 @@ if __name__ == '__main__':
     try:
         server = ThreadedHTTPServer(('localhost', port), Handler)
         print('Starting server, use <Ctrl-C> to stop')
-        webbrowser.open("localhost:" + str(port))
+        print('http://localhost:' + str(port))
         server.serve_forever()
-    except:
-        webbrowser.open("localhost:" + str(port))
+    except OSError:
+        print('Server already running!')
+        print('http://localhost:' + str(port))
+        sys.exit()
+    except KeyboardInterrupt:
+        clear()
+        sys.exit()
